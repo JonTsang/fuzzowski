0555a5affd49f5442c673a1dbe4de7a0252fa4d9
zengyongrui <zengyongrui@qianxin.com>
2020-10-15 19:16:18 +0800

    update dhcp fuzzer

--- fuzzowski/fuzzers/dhcp.py
+++ fuzzowski/fuzzers/dhcp.py
@@ -1,6 +1,8 @@
+import os
 from .ifuzzer import IFuzzer
 from fuzzowski import Session
 from fuzzowski.mutants.spike import *
+from uuid import getnode
 
 """
 reference:
@@ -51,109 +53,114 @@ class DHCP(IFuzzer):
 
         s_initialize('boot_request')
         s_static(b'\x01', name='message_type')
-        s_group(b'\x01', name='hareware_type', values=HARDWARE_TYPE)
-        s_byte(b'\x06', name='hardware_address_len', fuzzable=True)
-        s_byte(b'\x00', name='hop', fuzzable=True)
-        s_random('0xdeadbeef', min_length=32, max_length=32, name='transaction_id', fuzzable=False)
-        s_random('0x1234', min_length=16, max_length=16, name='seconds_elapsed', fuzzable=False)
-        s_group(b'\x00\x00', name='flags', values=[b'\x80\x00', b'\x00\x00'])
+        #s_group(b'\x01', name='hareware_type', values=HARDWARE_TYPE)
+        s_static(b'\x01', name='hareware_type')
+        s_byte(b'\x06', name='hardware_address_len', fuzzable=False)
+        s_byte(b'\x00', name='hop', fuzzable=False)
+        s_static(b'\xde\xad\xbe\xef', name='transaction_id')
+        s_static(b'\x02\x34', name='seconds_elapsed')
+        #s_random('\xde\xad\xbe\xef', min_length=4, max_length=4, name='transaction_id', fuzzable=True)
+        #s_random('\x12\x34', min_length=2, max_length=2, name='seconds_elapsed', fuzzable=False)
+        #s_group(b'\x00\x00', name='flags', values=[b'\x80\x00', b'\x00\x00'])
+        s_static(b'\x00\x00', name='flags')
         s_dword(0x00000000, name='client_ip', fuzzable=False)
         s_dword(0x00000000, name='your_client_ip', fuzzable=False)
         s_dword(0x00000000, name='next_server_ip', fuzzable=False)
         s_dword(0x00000000, name='rely_agent_ip', fuzzable=False)
-        s_static(b'\x00\x0c\x29\x7f\xa9\x03', name='client_mac')
-        s_static(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', name='client_mac_padding')
+        s_macaddr(name='mac_address')
+        s_static(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', name='mac_address_padding')
         s_string('dhcpclient', name='servername', size=64)
         s_string('dhcpclient', name='boot_filename', size=128)
+
+        #s_size("optiond", output_format="ascii", signed=True, fuzzable=True, name='Content-Length_size')
+
         s_static(b'\x63\x82\x53\x63', name='magic_cookie')
 
+
+        #Host Name Option
+        '''
+        byte = os.urandom(1)
+        n = int.from_bytes(byte, 'big')
+        s_static(b'\x0c')
+        s_static(os.urandom(1))
+        s_string('a' * n, size=n)
+        '''
         #Requested IP Address
-        with s_block('request_ip'):
-            s_static(b'\x32')
-            s_static(b'\x04')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x32')
+        s_static(b'\x04')
+        s_string('aaaa', size=0x04)
 
         #IP Address Lease Time
-        with s_block('ip_lease_time'):
-            s_static(b'\x33')
-            s_static(b'\x04')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x33')
+        s_static(b'\x04')
+        s_string('aaaa', size=0x04)
 
+        """
         #Option Overload
-        with s_block('option_overload'):
-            s_static(b'\x34')
-            s_static(b'\x01')
-            s_group(b'\x01', values=[1, 2, 3])
+        s_static(b'\x34')
+        s_static(b'\x01')
+        s_group(b'\x01', values=[1, 2, 3])
+        """
 
         #DHCP message type
-        with s_block('dhcp_message_type'):
-            s_static(b'\x35')
-            s_static(b'\x01')
-            s_group(b'\x01',values=[1,2,3,4,5,7,8,9])
+        s_static(b'\x35')
+        s_static(b'\x01')
+        s_group(b'\x01',values=[1,2,3,4,5,7,8,9])
 
         #Server Identifier
-        with s_block('server_identifier'):
-            s_static(b'\x36')
-            s_static(b'\x04')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x36')
+        s_static(b'\x04')
+        s_string('aaaa', size=0x04)
 
         #Parameter Request List
-        with s_block('request_list'):
-            s_static(b'\x37')
-            s_byte(b'\x00')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x37')
+        s_byte(b'\x07')
+        with s_block(name='request_list'):
+            s_byte(b'\x01')
+        s_repeat('request_list', name='repeat_reqestlist', min_reps=0, max_reps=7, step=7)
 
         #Message
-        with s_block('message'):
-            s_static(b'\x38')
-            s_static(b'\x00')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x38')
+        s_byte(b'\x38')
+        s_string('aaaa')
 
         #Maximum DHCP Message Size
-        with s_block('max_message_size'):
-            s_static(b'\x39')
-            s_static(b'\x02')
-            s_string('aa', size=0x02)
+        s_static(b'\x39')
+        s_byte(b'\xff')
+        s_string('aaaa')
 
         #Renewal (T1) Time Value
-        with s_block('remewal_value'):
-            s_static(b'\x3a')
-            s_static(b'\x04')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x3a')
+        s_static(b'\x04')
+        s_string('aaaa')
 
         #Rebinding (T2) Time Value
-        with s_block('rebinding_value'):
-            s_static(b'\x3b')
-            s_static(b'\x04')
-            s_string('aaaa', size=0x04)
+        s_static(b'\x3b')
+        s_static(b'\x04')
+        s_string('aaaa')
 
         #Vendor class identifier
-        with s_block('vendor_identifier'):
-            s_static(b'\x3c')
-            s_static(b'\x00')
-            s_string("aaa")
+        s_static(b'\x3c')
+        s_random(b'\x01', min_length=1, max_length=1)
+        s_string("aaa", fuzzable=True)
 
         #Client-identifier
-        with s_block('client-identifier'):
-            s_static(b'\x3d')
-            s_static(b'\x00')
-            s_string("aaa")
+        s_static(b'\x3d')
+        s_static(b'\x00')
+        s_string("aaa")
 
         #TFTP server name
-        with s_block('tftp_server_name'):
-            s_static(b'\x42')
-            s_byte(b'\x00')
-            s_string(b'\x00', )
+        s_static(b'\x42')
+        s_byte(b'\x00')
+        s_string(b'\x00', )
 
         #Bootfile name
-        with s_block('bootfile_name'):
-            s_static(b'\x43')
-            s_byte(b'\x00')
-            s_string(b'\x00')
+        s_static(b'\x43')
+        s_byte(b'\x00')
+        s_string(b'\x00')
 
         #Bootfile name
-        with s_block('end'):
-            s_static(b'\xff\x00\x00\x00')
+        s_static(b'\xff\x00\x00\x00')
 
         # --------------------------------------------------------------- #
 
